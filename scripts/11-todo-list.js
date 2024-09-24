a9993cd1714d913076fad484372e7a779cb68439
Pradeep  H V <hvpradeep92@gmail.com>
2024-09-24 11:30:53 +0530

    chapter 11, todolist part 2

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,12 +1,29 @@
 const todoList=[];
 
+
+
+function renderTodoList()
+{
+    let todoListHtml='';
+    for(let i=0;i<todoList.length;i++)
+        {
+            const todo=todoList[i];
+            const html=`<p>${todo}</p>`;
+            todoListHtml+=html;
+        }
+        console.log(todoListHtml)
+        document.querySelector('.js-todo-list').innerHTML=todoListHtml;
+}
+
 function addTodo(){
     const inputElement= document.querySelector('.js-name-input');
 
    const name=inputElement.value;
    inputElement.value='';
-   
+
    todoList.push(name);
    console.log(todoList);
+    renderTodoList();
 
 }
+
