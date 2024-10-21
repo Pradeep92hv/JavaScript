522f0d53c85dee09937ac9c04c1d806b17c13634
Pradeep92hv <hvpradeep92@gmail.com>
2024-10-21 13:52:57 +0530

    saved and genarated the Html for checkout

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,14 @@
-export const cart=[];
+export const cart=[
+  {
+    productId :"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  },
+  {
+    productId: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    quantity:1
+  }
+
+];
 
 export function addToCart(productId){
     let matchingItem='';
